c920e6e8647066ba0447e78e6a3d8d2ddf118021
Laku Jackson <llakujack@outlook.com>
2022-07-08 21:20:21 +1000

    client side setup

---
+++ TrickingLibraryAPI/Controllers/Home.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrickingLibraryAPI.Controllers
+{
+    [ApiController]
+    [Route("api/home")]
+    public class Home : ControllerBase //Controllersbase gives us API
+    {
+        [HttpGet]
+        public string Index()
+        {
+            return "Hello World";
+        }
+        
+    }
+}
